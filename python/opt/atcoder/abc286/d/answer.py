50fe750d16c7c6d3833d7b87026c4a2399a31f59
e-y555 <e.y555acm.uw@gmail.com>
2023-01-22 00:17:54 +0900

    abc286 d

---
+++ python/opt/atcoder/abc286/d/answer.py
@@ -0,0 +1,17 @@
+N, X = map(int, input().split())
+A = []
+B = []
+for _ in range(N):
+    a, b = map(int, input().split())
+    A.append(a)
+    B.append(b)
+
+dp = [[False for _ in range(X+1)] for _ in range(N+1)]
+dp[0][0] = True
+for i in range(N):
+    for j in range(X+1):
+        for k in range(B[i]+1):
+            if j >= A[i] * k:
+                if dp[i][j - A[i] * k]:
+                    dp[i+1][j] = True
+print('Yes' if dp[N][X] else 'No')
